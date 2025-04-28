dc60d71cedf1b52813339af78a354fdbe1a41784
Bowen L <bliu3535@gmail.com>
2025-04-28 15:11:24 -0700

    blabla

--- src/CountDownTimer.jsx
+++ src/CountDownTimer.jsx
@@ -1,1 +1,50 @@
-export default function CountDownTimer() {}
+import { useState, useEffect, useRef } from 'react';
+
+let ORIGINAL_COUNT = 60;
+let timer;
+
+export default function CountDownTimer() {
+	const [count, setCount] = useState(ORIGINAL_COUNT);
+	const [isCounting, setIsCounting] = useState(false);
+	// better to use ref since module-scoped varialbe only works if there's only one instance
+	// const timer = useRef(null);
+	// clearInterval(timer.current)
+
+	useEffect(() => {
+		if (!isCounting) return;
+
+		timer = setInterval(() => {
+			setCount(prev => prev - 1);
+		}, 1000);
+
+		return () => clearInterval(timer);
+	}, [isCounting]);
+
+	const handleStart = () => {
+		setIsCounting(true);
+	};
+
+	return (
+		<>
+			<div>{count}</div>
+			<button onClick={handleStart}>Start</button>
+			<button
+				onClick={() => {
+					clearInterval(timer);
+					setIsCounting(false);
+				}}
+			>
+				Pause
+			</button>
+			<button
+				onClick={() => {
+					clearInterval(timer);
+					setCount(ORIGINAL_COUNT);
+					setIsCounting(false);
+				}}
+			>
+				Reset
+			</button>
+		</>
+	);
+}
